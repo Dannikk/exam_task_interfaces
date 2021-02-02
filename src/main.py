45dc9f58b0f70c72afe579c4c63a6df715338017
Dannikk <nikita.danilov0103@gmail.com>
2021-02-02 23:13:35 +0300

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,9 @@
+
+
+def func(arr):
+    res = []
+    for i in arr:
+        res.append(i % 2)
+    return res
+
+print(func([1, 2, 3, 4, 5]))
---
+++ src/test_public.py
@@ -0,0 +1,8 @@
+import unittest
+
+from src.main import func
+
+class Test(unittest.TestCase):
+    def test_main(self):
+        for number in func([0, 2, 4, 10]):
+            self.assertEqual(number, 0)
