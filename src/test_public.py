8e1ede6e7c012bffcd5c4503f94bc879ddd1a3c9
Dannikk <nikita.danilov0103@gmail.com>
2021-02-03 14:19:08 +0300

    Ready

--- src/test_public.py
+++ src/test_public.py
@@ -1,8 +1,9 @@
-# import unittest
-#
-# from src.main import func
-#
-# class Test(unittest.TestCase):
-#     def test_main(self):
-#         for number in func([0, 2, 4, 10]):
-#             self.assertEqual(number, 0)
+import unittest
+
+from src import directory_reader as dr
+
+class Test(unittest.TestCase):
+    def test_main(self):
+        with dr.DirReader("data") as file_names:
+            for file_name in file_names:
+                self.assertTrue(len(file_name) < 11)
